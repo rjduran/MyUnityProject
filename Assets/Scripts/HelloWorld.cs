61bf56484c0295c170d91f4f758f845d0184239a
RJ Duran <rjduranjr@gmail.com>
2022-02-11 15:26:54 -0700

    Updated my unity project in prep for macOS build

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -13,6 +13,6 @@ public class HelloWorld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);  
     }
 }
